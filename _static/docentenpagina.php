5d82ffc19f37b45dfa9390242130d778de429970
bart314 <bart314@users.noreply.github.com>
2026-02-03 16:09:39 +0000

    deploy: 1820d92fb056e88512205631cabeda12311d490a

---
+++ _static/docentenpagina.php
@@ -0,0 +1,35 @@
+<?php
+$docenten  = [
+    [
+        "naam" => "Ralf van den Broek",
+        "vierlettercode" => "BRRA",
+        "vakken" => "Docent HBO-ICT, Software Engineering en dat soort dingen",
+        "avatar" => "TODO"
+    ],
+    [
+        "naam" => "Bart Barnard",
+        "vierlettercode" => "BABA",
+        "vakken" => "Docent HBO-ICT, Software Engineering en dat soort dingen",
+        "avatar" => "TODO"
+    ],
+    [
+        "naam" => "Harald Rietdijk",
+        "vierlettercode" => "RIHH",
+        "vakken" => "project webtech en dergelijke",
+        "avatar" => "TODO"
+    ],
+    [
+        "naam" => "Jacob de Boer",
+        "vierlettercode" => "BJAB",
+        "vakken" => "databases en dergelijke",
+        "avatar" => "TODO"
+    ],
+    [
+        "naam" => "Linda Noordhuis",
+        "vierlettercode" => "NOLI",
+        "vakken" => "Communicatie en dergelijke",
+        "avatar" => "TODO"
+    ]
+];
+
+echo "Er zitten " .count($docenten) . " docenten in het systeem";
